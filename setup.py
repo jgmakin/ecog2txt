18a2b9c1fdc5600713934fd1ec7bcb3f6410123e
J.G. Makin <jgmakin@gmail.com>
2020-03-03 17:10:35 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setuptools.setup(
     ],
     install_requires=[
         'numpy', 'scipy', 'matplotlib', 'pandas', 'seaborn',
-        'tikzplotlib', 'tensor2tensor==1.14.1', 'hickle', 'pickle',
+        'tikzplotlib', 'tensor2tensor==1.14.1', 'hickle',
         'python_speech_features', 'pyyaml', 'protobuf>=3.7',
         'tensorflow-probability>=0.7',
         # 'samplerate', 'tensorflow-gpu==1.14'
